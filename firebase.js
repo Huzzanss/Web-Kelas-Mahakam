4e2b45962ac90f47aedf0eb2031f1f8f92782579
Hafidzzz <huzzans.hafidz@gmail.com>
2026-01-31 17:27:39 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRMsEApnYGiloej-IYoY6mEpvMt6iRZlc",
+  authDomain: "chat-anonim-mahakam.firebaseapp.com",
+  databaseURL: "https://chat-anonim-mahakam-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "chat-anonim-mahakam",
+  storageBucket: "chat-anonim-mahakam.firebasestorage.app",
+  messagingSenderId: "270549776266",
+  appId: "1:270549776266:web:9f1eaf52c5aadb18577a73",
+  measurementId: "G-B2K5CCG9VL"
+};
 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
